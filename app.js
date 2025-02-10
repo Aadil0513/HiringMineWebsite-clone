504e1771d1271c57ee5ba2653762ca7bc64174d2
M Aadil Rajputecho <maadilrajput05@gmail.com>
2025-02-10 15:43:59 +0500

    LandingPage-complete

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log("hello world")
+
+
+// var num = +prompt("Enter a number")
+// var istrue = "false"
+// for(var  i =2 ; i<num ; i++){
+//     if(num % i === 0){
+//         istrue = "true"
+//         console.log("IT is not prime number")
+//         break ;
+//     }
+
+  
+// }
+
+// if(istrue === "false"){
+//     console.log("IT is prime number")
+
+// }
